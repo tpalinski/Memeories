e2137dce2b29dabf34c2c4f8765fda36108cd0cc
mrochny <trollklop@gmail.com>
2022-08-11 12:22:35 +0200

    Created sample preview for colour design stuff

---
+++ Memeories/Views/MainTabView.swift
@@ -0,0 +1,20 @@
+//
+//  MainTabView.swift
+//  Memeories
+//
+//  Created by Tymoteusz Palinski on 11/08/2022.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
