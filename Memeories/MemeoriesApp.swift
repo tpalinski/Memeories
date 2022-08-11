3325684989e293d81eab3615055c039a5c6ee07c
mrochny <trollklop@gmail.com>
2022-08-11 11:53:43 +0200

    Initial Commit

---
+++ Memeories/MemeoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemeoriesApp.swift
+//  Memeories
+//
+//  Created by Tymoteusz Palinski on 11/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct MemeoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
